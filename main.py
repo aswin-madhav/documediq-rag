dff006b2dd21dcdd8d1c663af76d8fae476a3617
aswin-madhav <83914989+aswin-madhav@users.noreply.github.com>
2025-03-23 11:55:33 +0530

    CORS used

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from rag import run_pipeline, index_document
 import uuid
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class InsertRequest(BaseModel):
     content: str
